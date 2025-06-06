5b051526d8c9bb7e4bf6a367e20ff6a20ee4b48d
NirmalKhadka <chattribeast@gmail.com>
2025-06-06 18:53:06 +0545

    Add server.js and update vercel.json for deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use('/tasks', router); // Mount JSON Server at /tasks
+server.use(router); // Serve db.json directly
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running');
 });
