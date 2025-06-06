b8bfee531bd306c560b8e975c87368c024bdaf3a
NirmalKhadka <chattribeast@gmail.com>
2025-06-06 17:06:32 +0545

    Add Vercel configuration

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/tasks';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/tasks';
 
 export const fetchTasks = async ({ pageParam = 1 }) => {
   try {
