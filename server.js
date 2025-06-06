0248c0d9c59595d3799762e35e6c43cb821ef9c8
NirmalKhadka <chattribeast@gmail.com>
2025-06-06 20:22:18 +0545

    Remove dist folder from repository

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import jsonServer from 'json-server';
+import fs from 'fs';
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router); // Serve db.json directly
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running');
-});
+server.use(router);
+
+export default (req, res) => {
+  if (!fs.existsSync('db.json')) {
+    res.status(500).json({ error: 'db.json not found' });
+    return;
+  }
+  server(req, res);
+};
